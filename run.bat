25e078884db19e2f4f060ab1113915b021418802
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-10-06 23:40:52 +0200

    ci: update run script

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-gprbuild -d -PC:\Users\Nasus\Desktop\Programowanie\Other\adapizza\adapizza.gpr C:\Users\Nasus\Desktop\Programowanie\Other\adapizza\src\simulation.adb
+gprbuild -d -Padapizza.gpr src\simulation.adb
 if errorlevel 1 goto end
  .\obj\simulation.exe
 :end
