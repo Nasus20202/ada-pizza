5683051bae3c22b045b8624312fc1032bdcdac5e
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-10-06 18:19:51 +0200

    ci: add run script

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+gprbuild -d -PC:\Users\Nasus\Desktop\Programowanie\Other\adapizza\adapizza.gpr C:\Users\Nasus\Desktop\Programowanie\Other\adapizza\src\simulation.adb
+@echo Run simulation:
+.\obj\simulation.exe
