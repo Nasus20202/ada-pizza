48c383db7d44870ffd964ba4491f0d3395d92436
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-10-06 22:51:10 +0200

    feat: conditional entry call

--- src/simulation.adb
+++ src/simulation.adb
@@ -1,7 +1,3 @@
--- A skeleton of a program for an assignment in programming languages
--- The students should rename the tasks of Suppliers, Clients, and the Fridge
--- Then, they should change them so that they would fit their assignments
--- They should also complete the code with constructions that lack there
 with Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Integer_Text_IO;
 with Ada.Numerics.Discrete_Random;
@@ -10,7 +6,8 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 procedure Simulation is
     type Ingredient_Type is
        (Dough, Cheese, Ham, Mushrooms, Tomato, Pepper, Pineapple);
-    type Pizza_Type is (Margherita, Capriciosa, Hawaii, Pepperoni);
+    type Pizza_Type is
+       (Margherita, Capriciosa, Hawaii, Pepperoni, Vegetariana, Cheesy);
     type Client_Type is (Student, Professor, Dean);
     package Random_Pizza is new Ada.Numerics.Discrete_Random (Pizza_Type);
 
@@ -24,22 +21,24 @@ procedure Simulation is
 
     -- Supplier produces determined ingredient
     task type Supplier_Task_Type is
-        -- Give the Supplier an identity, i.e. the ingredient type
         entry Start (Ingredient : in Ingredient_Type);
     end Supplier_Task_Type;
 
-    -- Client gets an arbitrary assembly of several ingredients from the Fridge
+    -- Client gets pizza cooked from several ingredients from the Fridge
     task type Client_Task_Type is
-        -- Give the Client an identity
         entry Start (Client : in Client_Type);
     end Client_Task_Type;
 
-    -- In the Fridge, ingredients are assemblied into an assembly
+    -- In the Fridge, ingredients are cooked into pizzas
     task type Fridge_Task_Type is
         -- Accept a ingredient to the storage provided there is a room for it
-        entry Take (Ingredient : in Ingredient_Type; Number : in Natural; Accepted: out Boolean);
-        -- Deliver an assembly provided there are enough ingredients for it
-        entry Deliver (Pizza : in Pizza_Type; Number : out Natural; Accepted: out Boolean);
+        entry Insert
+           (Ingredient : in     Ingredient_Type; Number : in Natural;
+            Accepted   :    out Boolean);
+        -- Cook and deliver a pizza provided there are enough ingredients for it
+        entry Deliver
+           (Pizza    : in     Pizza_Type; Number : out Natural;
+            Accepted :    out Boolean);
     end Fridge_Task_Type;
 
     Supplier_Tasks : array (Ingredient_Type) of Supplier_Task_Type;
@@ -53,67 +52,70 @@ procedure Simulation is
     end Log;
 
     task body Supplier_Task_Type is
-        subtype Delay_Time_Range is Integer range 4 .. 6;
+        subtype Delay_Time_Range_Millis is Integer range 4_000 .. 7_000;
         package Random_Delay is new Ada.Numerics.Discrete_Random
-           (Delay_Time_Range);
-        Generator           :
-           Random_Delay.Generator;   --  generator liczb losowych
+           (Delay_Time_Range_Millis);
+        Generator           : Random_Delay.Generator;
         Produced_Ingredient : Ingredient_Type;
         Counter             : Natural;
-		Accepted			: Boolean := False;
-		Retry_Delay			: Float := 1.0;
+        Accepted            : Boolean := False;
+        Retry_Delay         : Float   := 1.0;
     begin
         accept Start (Ingredient : in Ingredient_Type) do
-            Random_Delay.Reset
-               (Generator);    --  start random number generator
+            Random_Delay.Reset (Generator);
             Counter             := 1;
             Produced_Ingredient := Ingredient;
         end Start;
-        Log ("Supplier " & To_String (Produced_Ingredient), "Started supplier");
+        Log ("Supplier " & To_String (Produced_Ingredient),
+            "Started supplier");
         loop
-            delay Duration
-               (Random_Delay.Random (Generator)); --  symuluj produkcję
+            delay Duration (Float (Random_Delay.Random (Generator)) / 1_000.0);
             Log ("Supplier " & To_String (Produced_Ingredient),
                 "Produced ingredient " & To_String (Produced_Ingredient) &
                 " number " & To_String (Counter));
-            -- Accept for storage
-			while not Accepted loop
-            	Fridge_Task.Take (Produced_Ingredient, Counter, Accepted);
-				delay Duration(Retry_Delay);
-			end loop;
-            Counter := Counter + 1;
-			Accepted := False;
+
+            while not Accepted loop
+                select
+                    Fridge_Task.Insert
+                       (Produced_Ingredient, Counter, Accepted);
+                else
+                    Log ("Supplier " & To_String (Produced_Ingredient),
+                        "Fridge is busy, waiting seconds");
+                end select;
+                if not Accepted then
+                    delay Duration (Retry_Delay);
+                end if;
+            end loop;
+            Counter  := Counter + 1;
+            Accepted := False;
         end loop;
     end Supplier_Task_Type;
 
     task body Client_Task_Type is
-        subtype Consumption_Time_Range is Positive range 6 .. 8;
+        subtype Consumption_Time_Range_Millis is Positive range 6_000 .. 8_000;
         package Random_Consumption is new Ada.Numerics.Discrete_Random
-           (Consumption_Time_Range);
-        Time_Generator  :
-           Random_Consumption.Generator;  --  random number generator (time)
-        Pizza_Generator : Random_Pizza.Generator;    --  also (assemblies)
+           (Consumption_Time_Range_Millis);
+        Time_Generator  : Random_Consumption.Generator;
+        Pizza_Generator : Random_Pizza.Generator;
         Client_Name     : Client_Type;
         Counter         : Natural;
         Pizza           : Pizza_Type;
-		Accepted		: Boolean;
+        Accepted        : Boolean;
     begin
         accept Start (Client : in Client_Type) do
-            Random_Consumption.Reset (Time_Generator);   --  ustaw generator
-            Random_Pizza.Reset (Pizza_Generator);     --  też
+            Random_Consumption.Reset (Time_Generator);
+            Random_Pizza.Reset (Pizza_Generator);
             Client_Name := Client;
         end Start;
         Log ("Client " & To_String (Client_Name), "Started client");
         loop
             delay Duration
-               (Random_Consumption.Random
-                   (Time_Generator)); --  simulate consumption
+               (Float (Random_Consumption.Random (Time_Generator)) / 1_000.0);
             Pizza := Random_Pizza.Random (Pizza_Generator);
-            -- take an assembly for consumption
             Fridge_Task.Deliver (Pizza, Counter, Accepted);
             if Accepted then
                 Log ("Client " & To_String (Client_Name),
-                    "took pizza " & To_String (Pizza) & " number " &
+                    "Received pizza " & To_String (Pizza) & " number " &
                     To_String (Counter));
             else
                 Log ("Client " & To_String (Client_Name),
@@ -123,20 +125,23 @@ procedure Simulation is
     end Client_Task_Type;
 
     task body Fridge_Task_Type is
-        Storage_Capacity : constant Positive := 30;
+        Storage_Capacity : constant Positive := 50;
         type Storage_type is array (Ingredient_Type) of Natural;
         Storage                : Storage_type := (others => 0);
         Pizza_Recipes : array (Pizza_Type, Ingredient_Type) of Natural :=
-           (Margherita => (Dough => 1, Cheese => 1, Tomato => 2, others => 0),
-            Capriciosa =>
-               (Dough => 1, Cheese => 1, Ham => 2, Mushrooms => 2, Tomato => 1,
+           (Margherita  => (Dough => 1, Cheese => 1, Tomato => 1, others => 0),
+            Capriciosa  =>
+               (Dough => 1, Cheese => 1, Ham => 2, Mushrooms => 2, Tomato => 2,
                 others => 0),
-            Hawaii     =>
-               (Dough => 1, Cheese => 1, Ham => 2, Pineapple => 1, Tomato => 1,
+            Hawaii      =>
+               (Dough => 1, Cheese => 1, Ham => 1, Pineapple => 1, Tomato => 1,
                 others => 0),
-            Pepperoni  =>
-               (Dough  => 1, Cheese => 1, Pepper => 2, Tomato => 1,
-                others => 0));
+            Pepperoni   =>
+               (Dough => 1, Cheese => 1, Pepper => 3, Ham => 1, others => 0),
+            Vegetariana =>
+               (Dough  => 1, Cheese => 1, Mushrooms => 2, Tomato => 2,
+                others => 0),
+            Cheesy      => (Dough => 1, Cheese => 3, others => 0));
         Max_Ingredient_Content : array (Ingredient_Type) of Natural;
         Counters : array (Pizza_Type) of Natural := (others => 1);
         Ingredients_In_Storage : Natural := 0;
@@ -157,12 +162,12 @@ procedure Simulation is
         end Setup_Variables;
 
         function Can_Accept (Ingredient : Ingredient_Type) return Boolean is
-            Free         : Natural;         --  free room in the storage
+            Free         : Natural;
             -- how many ingredients are for ingrediention of arbitrary assembly
             Lacking      : array (Ingredient_Type) of Natural;
             -- how much room is needed in storage to produce arbitrary assembly
             Lacking_room : Natural;
-            Result       : Boolean;                   --  can accept
+            Result       : Boolean;
         begin
             if Ingredients_In_Storage >= Storage_Capacity then
                 return False;
@@ -176,31 +181,21 @@ procedure Simulation is
                 end if;
             end loop;
             if Result then
-                return
-                   True;                --  storage has ingredients for arbitrary
-                --  assembly
-            end if;
-            if Integer'Max
-                  (0,
-                   Max_Ingredient_Content (Ingredient) -
-                   Storage (Ingredient)) >
-               0
-            then
-                -- exactly this ingredient lacks
                 return True;
             end if;
-            Lacking_room :=
-               1;                     --  insert current ingredient
+            if Integer'Max (0, Max_Ingredient_Content (Ingredient) - Storage (Ingredient)) > 0
+            then
+                return True;
+            end if;
+            Lacking_room := 1;
             for I in Ingredient_Type loop
                 Lacking (I)  :=
                    Integer'Max (0, Max_Ingredient_Content (I) - Storage (I));
                 Lacking_room := Lacking_room + Lacking (I);
             end loop;
             if Free >= Lacking_room then
-                -- there is enough room in storage for arbitrary assembly
                 return True;
             else
-                -- no room for this ingredient
                 return False;
             end if;
         end Can_Accept;
@@ -220,45 +215,65 @@ procedure Simulation is
         begin
             Append (Message, "Ingredients: [ ");
             for I in Ingredient_Type loop
-                Append
-                   (Message,
+                Append (Message,
                     To_String (I) & ": " & To_String (Storage (I)) & " ");
             end loop;
             Append (Message, "]");
             Log ("Fridge", To_String (Message));
         end Storage_Contents;
 
+        procedure Clear_Storage is
+            type Maximal_Ingredient_Count_Type is
+               array (Ingredient_Type) of Natural;
+            Maximal_Ingredient_Count : Maximal_Ingredient_Count_Type :=
+               (Dough => 9, Cheese => 9, Ham => 9, Tomato => 9, others => 6);
+            Remove_Count             : Integer;
+        begin
+            for Ingredient in Ingredient_Type loop
+                if (Storage (Ingredient) >
+                    Maximal_Ingredient_Count (Ingredient))
+                then
+                    Remove_Count           := Storage (Ingredient) / 2;
+                    Storage (Ingredient)   :=
+                       Storage (Ingredient) - Remove_Count;
+                    Ingredients_In_Storage :=
+                       Ingredients_In_Storage - Remove_Count;
+                    Log ("Fridge", "Removed " & To_String (Remove_Count) & " " &
+                        To_String (Ingredient) & "S from storage");
+                end if;
+            end loop;
+        end Clear_Storage;
+
     begin
         Log ("Fridge", "Started");
         Setup_Variables;
         loop
             select
-                accept Take
-                   (Ingredient : in Ingredient_Type; Number : in Natural; Accepted : out Boolean)
+                accept Insert
+                   (Ingredient : in     Ingredient_Type; Number : in Natural;
+                    Accepted   :    out Boolean)
                 do
                     if Can_Accept (Ingredient) then
-                        Log ("Fridge",
-                            "Accepted ingredient " & To_String (Ingredient) &
+                        Log ("Fridge", "Accepted ingredient " & To_String (Ingredient) &
                             " number " & To_String (Number));
                         Storage (Ingredient)   := Storage (Ingredient) + 1;
                         Ingredients_In_Storage := Ingredients_In_Storage + 1;
+                        Clear_Storage;
                         Storage_Contents;
-						Accepted := True;
+                        Accepted := True;
                     else
-                        Log ("Fridge",
-                            "Rejected ingredient " & To_String (Ingredient) &
+                        Log ("Fridge", "Rejected ingredient " & To_String (Ingredient) &
                             " number " & To_String (Number));
-						Accepted := False;
+                        Accepted := False;
                     end if;
-                end Take;
-            else
-                null;
-            end select;
-            select
-                accept Deliver (Pizza : in Pizza_Type; Number : out Natural; Accepted : out Boolean) do
+                end Insert;
+            or
+                accept Deliver
+                   (Pizza    : in     Pizza_Type; Number : out Natural;
+                    Accepted :    out Boolean)
+                do
                     if Can_Deliver (Pizza) then
-                        Log ("Fridge",
-                            "Delivered pizza " & To_String (Pizza) &
+                        Log ("Fridge", "Delivered pizza " & To_String (Pizza) &
                             " number " & To_String (Counters (Pizza)));
                         for Ingredient in Ingredient_Type loop
                             Storage (Ingredient)   :=
@@ -271,14 +286,12 @@ procedure Simulation is
                         Number           := Counters (Pizza);
                         Counters (Pizza) := Counters (Pizza) + 1;
                         Storage_Contents;
-						Accepted := True;
+                        Accepted := True;
                     else
-                        Number := 0;
-						Accepted := False;
+                        Number   := 0;
+                        Accepted := False;
                     end if;
                 end Deliver;
-            else
-                null;
             end select;
         end loop;
     end Fridge_Task_Type;
